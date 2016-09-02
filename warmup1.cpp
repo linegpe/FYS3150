ad9e1b03c11a42370b0bf69ee02faa211172efe7
linegpe <line.gaard.pedersen@gmail.com>
2016-09-02 14:11:30 +0200

    Fixed something

--- warmup1.cpp
+++ warmup1.cpp
@@ -3,7 +3,7 @@
 # include <math.h>
 # include <string>
 # include <cmath>
-
+# include <fstream>
 using namespace std;
 
 double f(double x);
@@ -15,10 +15,19 @@ double totalfeil(double x, double h);
 int main(){
 	
 	double x = sqrt(2);
-	double h = 0.001; // Step length
-	cout << "Uten feilledd: " << derivert(x,h) << endl;
-	cout << "Med feilledd: " << derivert_mfeil(x,h) << endl;
-	cout << "Absoluttfeil: " << totalfeil(x,h) << endl;
+	ofstream myfile;
+	myfile.open("warmupresults.txt");
+	for (int tall = 1; tall<20; ++tall){
+		double h = 1./pow(10, tall);
+		cout << "Steglengde " << h << " gir: " << endl;
+		cout << "Uten feilledd: " << derivert(x,h) << endl;
+		cout << "Med feilledd: " << derivert_mfeil(x,h) << endl;
+		cout << "Absoluttfeil: " << totalfeil(x,h) << endl << endl;
+		myfile << log10(h) << " " << log(totalfeil(x,h)) << endl;
+
+	}
+	myfile.close();
+
 
 	return 0;
 }
@@ -42,13 +51,13 @@ double likn2(double x, double h) {
 }
 
 double derivert(double x, double h){
-	return (f(x+h) - f(x-h))/(2*h); // + o(h*h);
+	return (f(x+h) - f(x-h))/(2*h);
 }
 
 double derivert_mfeil(double x, double h){
-	return (f(x+h) - f(x-h))/(2*h) + o(h); // + o(h*h);
+	return (f(x+h) - f(x-h))/(2*h) + o(h);
 }
 
 double totalfeil(double x, double h){
-	return 1/3 - abs(derivert_mfeil(x,h));
+	return abs((1./3) - derivert_mfeil(x,h));
 }
