42e2c6435901af1680180e39048fdd88c6821d21
helenper <helen.persson91@gmail.com>
2016-09-16 16:25:44 +0200

    Updatet values and corrected plot

--- Project1/epsilonplot.py
+++ Project1/epsilonplot.py
@@ -1,10 +1,16 @@
-from numpy import linspace
+# This program plots the value of the relaive error against 
+# the corresponding N-values
+
+from numpy import linspace, log10
 import matplotlib.pyplot as plt
 
-epsilon = [-2.23297, -7.06964, -11.6932, -16.3003, -20.7334, -15.5917, -13.7288] # Wrong values! Helén, fix it D: 
-N = linspace(1,7,7)
 
-plt.plot(N,epsilon)
-plt.xlabel('ln(N)')
+epsilon = [-2.33482, - 7.06964,  -11.6933, -16.3003, -20.7334, -15.5917]
+N = [10, 100, 1000, 10000, 100000, 1000000]
+
+plt.plot(log10(N),epsilon, 'o--')
+plt.xlabel('log(N)')
 plt.ylabel('Error estimate $\epsilon$')
+
+
 plt.show()
