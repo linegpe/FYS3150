75d432794f0138fd74f5b03872d8f37b36b79b3f
linegpe <line.gaard.pedersen@gmail.com>
2016-09-28 13:43:24 +0200

    Fixed sorting function

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -84,18 +84,34 @@ int main(){
     cout << endl << "Run time: " << time << " sec." << endl << endl;
 	cout << endl << "Number of iterations: " << iterations << endl << endl;
 
+	double *neweigvals = new double[N];
 	// Sort eigenvalues:
 	double *eigenvalues = new double [N];
 	for (int i = 0; i < N; i++){
 		eigenvalues[i] = A[i][i];
+		neweigvals[i] = A[i][i];
 	}
 
 	vec eigval2 = zeros<vec>(N);
 
 	//double *neweigvals = new double [N];
+	//neweigvals = eigenvalues;
 	//double *v_sorted = new double [N];
+
+	cout << "Test 1: " << endl;
+	for (int i = 0; i < N; i++){
+		cout << eigenvalues[i] << endl;
+		cout << neweigvals[i] << endl << endl;
+	}
+
 	double *eigenvalues_sorted = new double[N];
 	eigenvalues_sorted = sort(eigenvalues, N, R);
+
+	cout << "Test 2: " << endl;
+	for (int i = 0; i < N; i++){
+		cout << eigenvalues[i] << endl;
+		cout << neweigvals[i] << endl << endl;
+	}
 
 	cout << "Eigenvalues sorted by Sunniva: " << endl;
 	for (int i = 1; i < 4; i++) // First element 0
@@ -104,24 +120,28 @@ int main(){
 	}
 
 	// Print 3 first eigenvalues
-	cout << "Eigenvalues: " << endl;
-	for (int i = 0; i < N; i++){
-		eigval2(i) = eigenvalues[i];
-	}
-	eigval2 = sort(eigval2);
-	cout << eigval2(0) << endl;
-	cout << eigval2(1) << endl;
-	cout << eigval2(2) << endl;
+	// cout << "Eigenvalues: " << endl;
+	// for (int i = 0; i < N; i++){
+	// 	eigval2(i) = eigenvalues[i];
+	// }
+	// eigval2 = sort(eigval2);
+	// cout << eigval2(0) << endl;
+	// cout << eigval2(1) << endl;
+	// cout << eigval2(2) << endl;
 
 	//Find index of eigenvalues
 	int index0;
 	int index1;
 	int index2;
-	double eps2 = 0.0001;
+	double eps2 = 0.1;
 	for (int i = 0; i < N; i++){
-		if (fabs(eigenvalues[i] - eigenvalues_sorted[1]) < eps2) {index0 = i;}
-		if (fabs(eigenvalues[i] - eigenvalues_sorted[2]) < eps2) {index1 = i;}
-		if (fabs(eigenvalues[i] - eigenvalues_sorted[3]) < eps2) {index2 = i;}
+		cout << "Testing..." << endl;
+		cout << fabs(eigenvalues[i] - eigenvalues_sorted[1]) << endl;
+		cout << eigenvalues[i] << endl;
+		cout << neweigvals[i] << endl;
+		if (fabs(neweigvals[i] - eigenvalues_sorted[1]) < epsilon) {index0 = i;}
+		if (fabs(neweigvals[i] - eigenvalues_sorted[2]) < epsilon) {index1 = i;}
+		if (fabs(neweigvals[i] - eigenvalues_sorted[3]) < epsilon) {index2 = i;}
 	}
 	cout << "Indexes: " << index0 << " " << index1 << " " << index2 << endl;
 
