d5da749951e761cd2ea9b4628ad390da08897e47
helenper <helenpersson@1x-193-157-199-83.uio.no>
2016-09-29 12:37:03 +0200

    fixed plot2e.py

--- Project2/plot1e.py
+++ Project2/plot1e.py
@@ -16,32 +16,25 @@ f.close()
 
 N = len(r1)
 rho = np.linspace(0,5,N)
-#plt.show(block=False)
+
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
 fig = plt.figure()
 
-
 fig1 = fig.add_subplot(311)
 fig1.plot(rho,r1,label='1st eigenvector', color='indigo')
 plt.legend()
-
 
 fig2 = fig.add_subplot(312)
 fig2.plot(rho,r2,label='2nd eigenvector', color='fuchsia')
 plt.legend()
 plt.ylabel('Probability $|R(r)|$', fontsize=15)
 
-
-
-
 fig3 = fig.add_subplot(313)
 fig3.plot(rho,r3,label='3rd eigenvector', color='violet')
 plt.legend()
 plt.xlabel('Distance $ \\rho $', fontsize=15)
-
-
 
 
 show = raw_input("Show figure now? [y/n] ")
