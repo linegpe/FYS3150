2536eb7edb7e15d70e2ad58db6131b6df22bfe3d
sunnivja <sunnivja@student.matnat.uio.no>
2016-09-29 12:23:06 +0200

    Testprogram
    Tester ortogonalitet, max_off_diag og sorteringsfunksjon. Kan kjøre run_all_tests()  i et separat program for å kjøre testene for en enkel matrise.

---
+++ Project2/testprogram.h
@@ -0,0 +1,12 @@
+#ifndef __TESTPROGRAM_H_INCLUDED__
+#define __TESTPROGRAM_H_INCLUDED__
+
+double* sort(double *v, int n, double** R);
+void test_highest_element(double** A, int n, double amax);
+//double* test_armadillo(double** A, int n);
+void test_orthogonality(double**A, int n);
+double mean(double*v, int n);
+void test_sort(double*v, int n);
+void run_all_tests(int a);
+//double** conv_to_arma(double** A, int n);
+#endif
