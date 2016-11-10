ac93a6fff75b6d53cf9e2621f6861477843138fc
linegpe <line.gaard.pedersen@gmail.com>
2016-11-10 11:26:17 +0100

    Fixed chi and cv

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-// How to run from terminal:
+// How to run from terminal (assuming 4 cores):
 
 // mpic++ -O3 -o main.x main.cpp
 // mpirun -n 4 ./main.x
@@ -15,7 +15,6 @@
 using namespace std;
 
 void printSpinMatrix(int L, double** spins);
-void createRandomSpins(int L, double** spins);
 double calculateEnergy(int size, double J, double** spinMatrix);
 double calculateMagnetization(int L, double **spinMatrix);
 void metropolisAlgorithm(double T, double& E, double& M, double& E2, double& M2, double& cv, double& chi, int L, int N, double J, double** spinMatrix, string filename);
@@ -23,16 +22,18 @@ void printResults(double L, double T, double N, double energy, double E2, double
 void normalize(int numprocs, double T, int L, int N, double& E, double& E2, double& M, double& M2, double& cv, double& chi);
 double** createRandomMatrix(int L);
 double** createOrderedMatrix(int L);
-//void writeResultToFile(string myfile, double T, double E, double M, double E2, double M2, double cv, double chi);
 
 
 int main(int nargs, char* args[]){
-	int L = 2;
-	double J = -1.0;
-	int N = 1e2; // Number of Monte Carlo Cycles
+	int L = 40;			// Dimension of lattice      //CHANGE
+	double J = -1.0;	// Energy constant with unit
+	int N = 10000; 	// Number of Monte Carlo Cycles  //CHANGE TO 100 000
+
+	// Choose random or ordered spin lattice:
 	string initialMode = "random";
 	//string initialMode = "ordered";
 
+	// Set up the matrix:
 	double** spins;
 	if(initialMode == "random") {
 		spins = createRandomMatrix(L);
@@ -40,27 +41,38 @@ int main(int nargs, char* args[]){
 		spins = createOrderedMatrix(L);
 	}
 
+	// For code paralellization:
 	int numprocs, my_rank;
     MPI_Init (&nargs, &args);
     MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
 
-
-
-    //MPI_Reduce(&local_sum, &total_sum, 1, MPI_Double, MPI_SUM, 0, MPI_COMM_WORLD);
-
+    // Make pseudo-random numbers as random as possible:
     srand (time(NULL)+my_rank);
-    // double E, M, E2, M2, cv, chi;
-	// metropolisAlgorithm(temp, E, M, E2, M2, cv, chi, L, N, J, spins);
-	// printResults(L,temp,N*numprocs,E,M,E2,M2,cv,chi);
+    //srand (time(NULL));
 
-    for (double temp = 1; temp <= 1; temp += 0.05){
+    // For running non-parallelized:
+/*    double temp = 1.0;
+	double E, M, E2, M2, cv, chi;
+	//sprintf(filename, "expect_%s_T%.2f.dat", initialMode.c_str(), temp);
+	metropolisAlgorithm(temp, E, M, E2, M2, cv, chi, L, N, J, spins, "test");*/
+
+    // Write to file to study phase transitions:
+	ofstream myfile;									//UNCOMMENT
+	myfile.open("final_L20.dat");					//UNCOMMENT
+    
+    // Begin algorithm:
+    for (double temp = 2.1; temp <= 2.4; temp += 0.05){
+
+    	// Variables to fill with values:
 		double E, M, E2, M2, cv, chi;
 		double total_E = 0; 
 		double total_M = 0;
 		double total_E2 = 0;
 		double total_M2 = 0;
 		char filename[10000];
+
+		// Automatically generated filename of output:
 		sprintf(filename, "expect_%s_T%.2f.dat", initialMode.c_str(), temp);
 		metropolisAlgorithm(temp, E, M, E2, M2, cv, chi, L, N, J, spins, string(filename));
 		cout << setprecision(15) << "E: " << E << " " << total_E << endl;
@@ -70,18 +82,15 @@ int main(int nargs, char* args[]){
 		MPI_Reduce(&M2, &total_M2, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 		cout << "Total E: " << total_E << endl;
 		if (my_rank == 0){
-			// Calculate cv here
-			//void normalize(int numprocs, double T, int L, int N, double& E, double& E2, double& M, double& M2, double& cv, double& chi)
 			normalize(numprocs, temp,L,N,total_E,total_E2,total_M,total_M2,cv,chi);
 			printResults(L,temp,N*numprocs,total_E,total_M,total_E2,total_M2,cv,chi);
+			myfile << total_E/(L*L) << " " << total_M/(L*L) << " " << cv << " " << chi << " " << temp << endl;	//UNCOMMENT
 		}
 	}
+	myfile.close();		//UNCOMMENT
 	MPI_Finalize();
 	return 0;
 }
-
-
-
 
 
 
@@ -120,20 +129,6 @@ double calculateMagnetization(int L, double **spinMatrix){
 	return fabs(magnetization);
 }
 
-void createRandomSpins(int L, double **spins){
-	for (int i = 0; i < L; i++){
-		for (int j = 0; j < L; j++){
-			int x = rand() % 2;
-			
-			if(x == 1) {
-				spins[i][j] =  1;
-			} else {
-				spins[i][j] = -1;
-			}
-			
-		}
-	}
-}
 
 double** createRandomMatrix(int L){
 	double **spins = new double*[L];
@@ -178,9 +173,9 @@ void printSpinMatrix(int L, double** spins) {
 
 void metropolisAlgorithm(double T, double& E, double& M, double& E2, double& M2, double& cv, double& chi, int L, int N, double J, double** spinMatrix, string filename){
 	// Wirte to file
-	ofstream myfile; 
+	//ofstream myfile; 						//COMMENT
 	//myfile.open(filename.c_str());
-	myfile.open("accepted.dat");
+	//myfile.open("accepted_ordered_T1.dat");			//COMMENT
 	// Calculate current energy
 	double energy = calculateEnergy(L,J,spinMatrix);
 	double magnetization = calculateMagnetization(L, spinMatrix); //, E2, M2;
@@ -225,7 +220,7 @@ void metropolisAlgorithm(double T, double& E, double& M, double& E2, double& M2,
 		mSquaredSum += magnetization*magnetization;
 		n += 1;
 
-		//myfile << accepted << endl;
+		//myfile << accepted << endl;		//COMMENT!
 		//myfile << energySum/((double)n*L*L) << " " << abs(magnetizationSum/((double) n*L*L)) << endl;
 		//myfile << energy << " " << magnetization << endl;
 	}
@@ -234,7 +229,7 @@ void metropolisAlgorithm(double T, double& E, double& M, double& E2, double& M2,
 	E2 = eSquaredSum;
 	M2 = mSquaredSum;
 
-	myfile.close();
+	//myfile.close();			// COMMENT!
 }
 
 void printResults(double L, double T, double N, double energy, double magnetization, double E2, double M2, double cv, double chi){
@@ -256,13 +251,11 @@ void normalize(int numprocs, double T, int L, int N, double& E, double& E2, doub
 	M = M/(numprocs*N);
 	M2 = M2/(numprocs*N);
 	cv = (E2 - E*E)/(T*T);
+	cv /= L*L;
 	chi = (M2 - M*M)/T;
+	chi /= L*L;
 	E /= L*L;
 	E2 /= L*L;
 	M /= L*L;
 	M2 /= L*L;	
 }
-
-// void writeResultToFile(string myfile, double T, double E, double M, double E2, double M2, double cv, double chi){
-// 	myfile << T << " " << E << " " << M << " " << E2 << " " << M2 << " " << cv << " " << chi << endl;
-// }
--- Project4/plot_accepted.py
+++ Project4/plot_accepted.py
@@ -2,30 +2,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data1 = np.loadtxt("accepted_random_T1.dat")
+#data1 = np.loadtxt("accepted.dat")
 data2 = np.loadtxt("accepted_ordered_T1.dat")
 data3 = np.loadtxt("accepted_random_T2_4.dat")
 data4 = np.loadtxt("accepted_ordered_T2_4.dat")
 
-randomT1 = data1[0::1]
-orederedT1 = data2[0::1]
-randomT24 = data3[0::1]
-orederedT24 = data4[0::1]
+randomT1 = data1[0::5]
+orederedT1 = data2[0::5]
+randomT24 = data3[0::5]
+orederedT24 = data4[0::5]
 
-N = len(randomT1)
-x = np.linspace(0,N,N)
+N1 = len(randomT1)
+N2 = len(orederedT1)
+x1 = np.linspace(0,N1,N1)
+x2 = np.linspace(0,N2,N2)
 
-plt.plot(x,randomT1,label="Random, T=1")
-plt.plot(x,orederedT1,label="Ordered, T=1")
+fig = plt.figure()
+
+labels = fig.add_subplot(111)
+labels.spines['top'].set_color('none')
+labels.spines['bottom'].set_color('none')
+labels.spines['left'].set_color('none')
+labels.spines['right'].set_color('none')
+labels.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+
 plt.ylabel("Total number of accepted configurations",fontsize=15)
-plt.xlabel("Number of Monte Carlo cycles",fontsize=15)
-plt.legend()
-plt.axis([-10000,1e6,0,3000])
-plt.show()
 
-plt.plot(x,randomT24,label="Random, T=2.4")
-plt.plot(x,orederedT24,label="Ordered, T=2.4")
-plt.ylabel("Total number of accepted configurations",fontsize=15)
+#fig.axes[0].get_yaxis()
+#ax = gca()
+#labels.yaxis.set_label_position("right")
 plt.xlabel("Number of Monte Carlo cycles",fontsize=15)
-plt.legend()
+
+fig1 = fig.add_subplot(211)
+fig1.plot(x1,randomT1,label="Random, T=1")
+fig1.plot(x2,orederedT1,label="Ordered, T=1")
+fig1.tick_params(axis='x', labelsize=12)
+fig1.tick_params(axis='y', labelsize=12)
+
+
+fig1.yaxis.tick_right()
+#plt.ylabel("Total number of accepted configurations",fontsize=15)
+#plt.xlabel("Number of Monte Carlo cycles",fontsize=15)
+plt.legend(loc="lower right")
+#plt.axis([-10000,1e6,0,3000])
+#plt.show()
+
+fig2 = fig.add_subplot(212)
+fig2.plot(x2,randomT24,label="Random, T=2.4")
+fig2.plot(x2,orederedT24,label="Ordered, T=2.4")
+fig2.yaxis.tick_right()
+fig2.tick_params(axis='x', labelsize=12)
+fig2.tick_params(axis='y', labelsize=12)
+#plt.ylabel("Total number of accepted configurations",fontsize=15)
+#plt.xlabel("Number of Monte Carlo cycles",fontsize=15)
+plt.legend(loc="lower right")#,bbox_transform=plt.gcf().transFigure)
 #plt.axis([-10000,1e6,0,50000])
 plt.show()
+
+
+#plt.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
