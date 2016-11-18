e2db678d9c4f4bb844d01ef1f2343f4cee2284f4
linegpe <line.gaard.pedersen@gmail.com>
2016-11-18 15:08:49 +0100

    Added savings

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -1,22 +1,23 @@
 #include <random>
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 // How to run from terminal:
 // g++ -std=c++11 main.cpp
 
 using namespace std;
 
+void sum_money(double* money,int nr_agents);
+
 int main(){
-	int nr_agents = 500;
-	int nr_transactions = 1e3; 	// Should be at least 10^7
-	int nr_simulations = 100; 	// Should be between 10^3 - 10^4
+	int nr_agents = 500;			// Should be 500
+	int nr_transactions = 1e7; 	// Should be at least 10^7
+	int nr_simulations = 1e3; 	// Should be between 10^3 - 10^4
 
 	int total_money = 0;
-	int max_initial_money = 100;
+	//int max_initial_money = 100;
+	double initial_money = 100;
+	double lambda = 0.25;
 
 	// Make array with agents
 	double* agents = new double[nr_agents];
@@ -28,30 +29,53 @@ int main(){
 	double* money = new double[nr_agents];
 
 	// Initial random distribution of money
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> intRNG(0, nr_agents-1);
+    uniform_real_distribution<double> doubleRNG(0,1);
 
-	//random_device epsilon();
-	//mt19937 gen(epsilon());
-	//uniform_int_distribution<> dis(0,nr_agents);
-	
-	for (int i = 0; i < total_money; i++){
-		money[i] = rand() % max_initial_money;
-		total_money += money[i];
+	//cout << "100 to everyone: " << endl;
+	for (int i = 0; i < nr_agents; i++){
+		money[i] = initial_money;
+		//cout << money[i] << endl;
 	}
+
+	sum_money(money,nr_agents);
 
 	// Metropolis algorithm for transactions
 	int completed_transactions = 0;
 	while (completed_transactions < nr_transactions){
 		// Pick two random agents
-		int i = rand() % nr_agents;
-		int j = rand() % nr_agents;
-		// Make sure i =/= j
-		while (i == j){
-			j = rand() % nr_agents;
-		}
-		double max = RAND_MAX;
-		double epsilon = rand() / ((double) max);
-		int new_money_i = epsilon*(money[i] + money[j]);
-		int new_money_j = (1-epsilon)*(money[i] + money[j]);
-		completed_transactions += 1;
+		int i = intRNG(gen);
+		int j = intRNG(gen);
+		if (i != j){
+
+    		double epsilon = doubleRNG(gen);
+    		double sum_ij = money[i]+money[j];
+			money[i] = lambda*money[i] + epsilon*(1.0-lambda)*sum_ij;
+			money[j] = lambda*money[j] + (1.0-epsilon)*(1.0-lambda)*sum_ij;
+			completed_transactions += 1;
+		} 
 	}
+
+
+	ofstream myfile;
+	myfile.open("final_money.dat");
+	for (int i = 0; i < nr_agents; i++){
+		myfile << money[i] << endl;
+	}
+
+
+	// cout << "Random distribution: " << endl;
+	// for (int i = 0; i < nr_agents; i++){
+	// 	//cout << money[i] << endl;
+	// }
+}
+
+void sum_money(double* money,int nr_agents){
+	double sum_money = 0;
+	for (int i = 0; i < nr_agents; i++){
+		sum_money += money[i];
+	}
+	cout << "SUM: " << sum_money << endl;
 }
